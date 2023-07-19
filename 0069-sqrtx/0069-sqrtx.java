60024aa12f07bfe43d1af9fa4ba6ee67d378447e
Aditya Goswami <139443833+coderadigo@users.noreply.github.com>
2023-07-19 17:30:12 +0530

    Time: 1 ms (100.00%), Space: 39.7 MB (76.81%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+       long lo =0;
+       long hi = x;
+       long ans =1;
+       while(hi>=lo){
+           long mid = (lo)+(hi-lo)/2;
+           long sq= mid*mid;
+           if(sq<=x){
+               ans = mid;
+               lo = mid+1;
+           }else{
+               hi = mid-1;
+           }
+       }
+       return (int)(ans);
+    }
+}
